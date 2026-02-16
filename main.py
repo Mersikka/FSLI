d5a36b9dbaf6835d3aa9f778f81236133fc1ced1
Mersikka <mio@argillander.me>
2026-02-16 19:56:06 +0200

    Add detection model files

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from fsli!")
+import cv2
+import mediapipe as mp
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import time
 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+HAND_MODEL_LOCATION = "./models/hand_landmarker.task"
+POSE_MODEL_LOCATION = "./models/pose_landmarker_lite.task"
+
+def main():
+    
 
 if __name__ == "__main__":
     main()
